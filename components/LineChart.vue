46b7d0e004625516cf8daa79b661d60d7f913241
nishimura87 <nishimura.k87@gmail.com>
2023-06-12 08:47:45 +0900

    Lineチャート修正

--- components/LineChart.vue
+++ components/LineChart.vue
@@ -1,17 +1,17 @@
 <script setup lang="ts">
-import { use } from 'echarts/core';
-import { CanvasRenderer } from 'echarts/renderers';
-import { LabelLayout } from 'echarts/features';
-import { LineChart } from 'echarts/charts';
+import { use } from "echarts/core";
+import { CanvasRenderer } from "echarts/renderers";
+import { LabelLayout } from "echarts/features";
+import { LineChart } from "echarts/charts";
 import {
   TitleComponent,
   TooltipComponent,
   LegendComponent,
   DataZoomComponent,
-} from 'echarts/components';
-import VChart from 'vue-echarts';
-import { ref, onMounted } from "vue";
-import 'echarts/lib/component/markLine';
+} from "echarts/components";
+import VChart from "vue-echarts";
+import { ref, watch } from "vue";
+import "echarts/lib/component/markLine";
 
 use([
   CanvasRenderer,
@@ -20,10 +20,10 @@ use([
   TooltipComponent,
   LegendComponent,
   LabelLayout,
-  DataZoomComponent
+  DataZoomComponent,
 ]);
 
-function getRandomInt(max:number) {
+function getRandomInt(max: number) {
   // ランダムな配列
   return Math.floor(Math.random() * Math.floor(max));
 }
@@ -36,6 +36,7 @@ const array5: number[] = [];
 const sample5: number[] = [];
 const max = 100;
 const length = 200;
+
 for (let i = 0; i < length; i++) {
   array1.push(getRandomInt(max));
 }
@@ -55,347 +56,353 @@ for (let i = 0; i < length; i++) {
   sample5.push(getRandomInt(max));
 }
 
-const sessionTime = 100;
+const sessionTime:number[] = [];
+for (let i = 0; i < length; i++) {
+  sessionTime.push(i);
+}
 
-const sliderValue:any = ref(0);
-onMounted(() => { 
-  //sliderValue.value = document.querySelector("#range");
-  sliderValue.value = document.getElementById('range');
-})
+const sliderValue = ref<number>(0);
+watch(sliderValue, () => {
+});
 
 const option = ref({
   title: {
-    text: 'LineChart',
-    left: 'center',
+    text: "LineChart",
+    left: "center",
     textStyle: {
-      color: '#fff',
-      lineHeight:30,
-    }
+      color: "#fff",
+      lineHeight: 30,
+    },
   },
-  tooltip:{
-    trigger:'axis',
-    axisPointer:{
-      type: 'cross'
+  tooltip: {
+    trigger: "axis",
+    axisPointer: {
+      type: "cross",
     },
   },
   axisPointer: {
     link: [
       {
-        xAxisIndex: 'all'
-      }
+        xAxisIndex: "all",
+      },
     ],
     label: {
-      backgroundColor: '#777'
-    }
+      backgroundColor: "#777",
+    },
   },
   xAxis: [
     {
-      type: 'category',
+      type: "category",
       boundaryGap: false,
+      axisLabel: { show: false },
+      axisTick: { show: false },
+      splitLine: { show: false },
+      gridIndex: 0,
+      data: sessionTime,
+    },
+    {
+      type: "category",
+      boundaryGap: false,
+      axisLabel: { show: false },
+      axisTick: { show: false },
+      splitLine: { show: false },
+      gridIndex: 1,
+      data: sessionTime,
+    },
+    {
+      type: "category",
+      boundaryGap: false,
+      axisLabel: { show: false },
+      axisTick: { show: false },
+      splitLine: { show: false },
+      gridIndex: 2,
+      data: sessionTime,
+    },
+    {
+      type: "category",
+      boundaryGap: false,
+      axisLabel: { show: false },
+      axisTick: { show: false },
+      splitLine: { show: false },
+      gridIndex: 3,
+      data: sessionTime,
+    },
+    {
+      type: "category",
+      boundaryGap: false,
+      axisLabel: { show: false },
+      axisTick: { show: false },
+      splitLine: { show: false },
+      gridIndex: 4,
+      data: sessionTime,
+    },
+    {
+      type: "category",
+      boundaryGap: false,
+      axisLabel: { show: false },
+      axisTick: { show: false },
+      splitLine: { show: false },
+      gridIndex: 5,
+      data: sessionTime,
+    },
+  ],
+  yAxis: [
+    {
+      type: "value",
+      data: array1,
+      axisLine: { show: false },
       axisLabel: { show: false },
       axisTick: { show: false },
       splitLine: { show: false },
       gridIndex: 0,
     },
     {
-      type: 'category',
-      boundaryGap: false,
+      type: "value",
+      data: array1,
+      axisLine: { show: false },
       axisLabel: { show: false },
       axisTick: { show: false },
       splitLine: { show: false },
       gridIndex: 1,
     },
     {
-      type: 'category',
-      boundaryGap: false,
+      type: "value",
+      data: array1,
+      axisLine: { show: false },
       axisLabel: { show: false },
       axisTick: { show: false },
       splitLine: { show: false },
       gridIndex: 2,
     },
     {
-      type: 'category',
-      boundaryGap: false,
+      type: "value",
+      data: array1,
+      axisLine: { show: false },
       axisLabel: { show: false },
       axisTick: { show: false },
       splitLine: { show: false },
       gridIndex: 3,
     },
     {
-      type: 'category',
-      boundaryGap: false,
+      type: "value",
+      data: array1,
+      axisLine: { show: false },
       axisLabel: { show: false },
       axisTick: { show: false },
       splitLine: { show: false },
       gridIndex: 4,
     },
     {
-      type: 'category',
-      boundaryGap: false,
+      type: "value",
+      data: array1,
+      axisLine: { show: false },
       axisLabel: { show: false },
       axisTick: { show: false },
       splitLine: { show: false },
       gridIndex: 5,
     },
   ],
-  yAxis: [
-    {
-      type: 'value',
-      data: array1,
-      axisLine: { show: false },
-      axisLabel: { show: false },
-      axisTick: { show: false },
-      splitLine: { show: false },
-      gridIndex: 0
-    },
-    {
-      type: 'value',
-      data: array1,
-      axisLine: { show: false },
-      axisLabel: { show: false },
-      axisTick: { show: false },
-      splitLine: { show: false },
-      gridIndex: 1
-    },
-    {
-      type: 'value',
-      data: array1,
-      axisLine: { show: false },
-      axisLabel: { show: false },
-      axisTick: { show: false },
-      splitLine: { show: false },
-      gridIndex: 2
-    },
-    {
-      type: 'value',
-      data: array1,
-      axisLine: { show: false },
-      axisLabel: { show: false },
-      axisTick: { show: false },
-      splitLine: { show: false },
-      gridIndex: 3
-    },
-    {
-      type: 'value',
-      data: array1,
-      axisLine: { show: false },
-      axisLabel: { show: false },
-      axisTick: { show: false },
-      splitLine: { show: false },
-      gridIndex: 4
-    },
-    {
-      type: 'value',
-      data: array1,
-      axisLine: { show: false },
-      axisLabel: { show: false },
-      axisTick: { show: false },
-      splitLine: { show: false },
-      gridIndex: 5
-    }
-  ],
   dataZoom: [
     {
-      type: 'inside',
+      type: "inside",
       start: 0,
       end: 100,
       height: 30,
-      xAxisIndex: [0,1,2,3,4,5]
+      xAxisIndex: [0, 1, 2, 3, 4, 5],
     },
     {
-      type: 'slider',
+      type: "slider",
       start: 0,
       end: 100,
       height: 30,
-      xAxisIndex: [0,1,2,3,4,5]
-    }
+      xAxisIndex: [0, 1, 2, 3, 4, 5],
+    },
   ],
   grid: [
     {
-    bottom: '10%',
-    left: 10,
-    right: 10,
-    height: '15%'
+      bottom: "10%",
+      left: 10,
+      right: 10,
+      height: "15%",
     },
     {
-    bottom: '27%',
-    left: 10,
-    right: 10,
-    height: '15%'
+      bottom: "27%",
+      left: 10,
+      right: 10,
+      height: "15%",
     },
     {
-    bottom: '44%',
-    left: 10,
-    right: 10,
-    height: '15%'
+      bottom: "44%",
+      left: 10,
+      right: 10,
+      height: "15%",
     },
     {
-    bottom: '61%',
-    left: 10,
-    right: 10,
-    height: '15%'
+      bottom: "61%",
+      left: 10,
+      right: 10,
+      height: "15%",
     },
     {
-    bottom: '77%',
-    left: 10,
-    right: 10,
-    height: '15%'
+      bottom: "77%",
+      left: 10,
+      right: 10,
+      height: "15%",
     },
     {
-    bottom: '77%',
-    left: 10,
-    right: 10,
-    height: '15%'
+      bottom: "77%",
+      left: 10,
+      right: 10,
+      height: "15%",
     },
   ],
   series: [
     {
-      name: 'Fake Data1',
-      type: 'line',
-      symbol: 'none',
-      sampling: 'lttb',
+      name: "Fake Data1",
+      type: "line",
+      symbol: "none",
+      sampling: "lttb",
       itemStyle: {
-        color: 'red'
+        color: "red",
       },
       data: array1,
       markLine: {
         animation: false,
-        symbol: 'none',
+        symbol: "none",
         label: {
-          show: false
+          show: false,
         },
         lineStyle: {
-          type: 'dashed',
-          color: 'red',
-          width: '3'
+          type: "dashed",
+          color: "red",
+          width: "3",
         },
-        data: [{ xAxis: sliderValue }]
-      }
+        data: [{ xAxis: sliderValue }],
+      },
     },
     {
-      name: 'Fake Data2',
-      type: 'line',
-      symbol: 'none',
-      sampling: 'lttb',
+      name: "Fake Data2",
+      type: "line",
+      symbol: "none",
+      sampling: "lttb",
       itemStyle: {
-        color: 'green'
+        color: "green",
       },
       data: array2,
       xAxisIndex: 1,
       yAxisIndex: 1,
       markLine: {
         animation: false,
-        symbol: 'none',
+        symbol: "none",
         label: {
-          show: false
+          show: false,
         },
         lineStyle: {
-          type: 'dashed',
-          color: 'red',
-          width: '3'
+          type: "dashed",
+          color: "red",
+          width: "3",
         },
-        data: [{ xAxis: sessionTime }]
-      }
+        data: [{ xAxis: sliderValue }],
+      },
     },
     {
-      name: 'Fake Data3',
-      type: 'line',
-      symbol: 'none',
-      sampling: 'lttb',
+      name: "Fake Data3",
+      type: "line",
+      symbol: "none",
+      sampling: "lttb",
       itemStyle: {
-        color: 'pink'
+        color: "pink",
       },
       data: array3,
       xAxisIndex: 2,
       yAxisIndex: 2,
       markLine: {
         animation: false,
-        symbol: 'none',
+        symbol: "none",
         label: {
-          show: false
+          show: false,
         },
         lineStyle: {
-          type: 'dashed',
-          color: 'red',
-          width: '3'
+          type: "dashed",
+          color: "red",
+          width: "3",
         },
-        data: [{ xAxis: sessionTime }]
-      }
+        data: [{ xAxis: sliderValue }],
+      },
     },
     {
-      name: 'Fake Data4',
-      type: 'line',
-      symbol: 'none',
-      sampling: 'lttb',
+      name: "Fake Data4",
+      type: "line",
+      symbol: "none",
+      sampling: "lttb",
       itemStyle: {
-        color: 'yellow'
+        color: "yellow",
       },
       data: array4,
       xAxisIndex: 3,
       yAxisIndex: 3,
       markLine: {
         animation: false,
-        symbol: 'none',
+        symbol: "none",
         label: {
-          show: false
+          show: false,
         },
         lineStyle: {
-          type: 'dashed',
-          color: 'red',
-          width: '3'
+          type: "dashed",
+          color: "red",
+          width: "3",
         },
-        data: [{ xAxis: sessionTime }]
-      }
+        data: [{ xAxis: sliderValue }],
+      },
     },
     {
-      name: 'Fake Data5',
-      type: 'line',
-      symbol: 'none',
-      sampling: 'lttb',
+      name: "Fake Data5",
+      type: "line",
+      symbol: "none",
+      sampling: "lttb",
       itemStyle: {
-        color: 'rgba(0, 0, 255, 1)'
+        color: "rgba(0, 0, 255, 1)",
       },
       data: array5,
       xAxisIndex: 4,
       yAxisIndex: 4,
       markLine: {
         animation: false,
-        symbol: 'none',
+        symbol: "none",
         label: {
-          show: false
+          show: false,
         },
         lineStyle: {
-          type: 'dashed',
-          color: 'red',
-          width: '3'
+          type: "dashed",
+          color: "red",
+          width: "3",
         },
-        data: [{ xAxis: sessionTime }]
-      }
+        data: [{ xAxis: sliderValue }],
+      },
     },
     {
-      name: 'Fake Data5',
-      type: 'line',
-      symbol: 'none',
-      sampling: 'lttb',
+      name: "Fake Data5",
+      type: "line",
+      symbol: "none",
+      sampling: "lttb",
       itemStyle: {
-        color: 'rgba(114, 114, 114, 0.7)'
+        color: "rgba(114, 114, 114, 0.7)",
       },
       data: sample5,
       xAxisIndex: 5,
       yAxisIndex: 5,
-    }
+    },
   ],
 });
 </script>
 
+
 <template>
   <div>
     <v-chart id="chart2" class="chart2" :option="option" autoresize />
-    <input id="range" v-model="sliderValue" type="range" min="0" max="200" class="slider" />
-    <input v-model="sliderValue" type="number" class="input" />
+    <input id="range" v-model.number="sliderValue" type="range" min="0" max="199" class="slider" />
   </div>
 </template>
-
 
 <style>
 .chart2 {
@@ -408,5 +415,10 @@ const option = ref({
 
 .example {
   color: #fff;
+}
+
+.white {
+  color: #fff;
+  font-weight: bold;
 }
 </style>
